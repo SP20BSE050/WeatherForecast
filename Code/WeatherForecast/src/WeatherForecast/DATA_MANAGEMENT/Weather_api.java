67384e3adaf754eb1c6935e2c40e92be416b945a
Syed Muhammad Saqib <92437076+SP20BSE050@users.noreply.github.com>
2021-12-22 14:16:07 +0500

    main

---
+++ Code/WeatherForecast/src/WeatherForecast/DATA_MANAGEMENT/Weather_api.java
@@ -0,0 +1,36 @@
+package WeatherForecast.DATA_MANAGEMENT;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+public class Weather_api {
+    public static String readFromWeb(String webURL) throws IOException {
+        URL url = new URL(webURL);
+        InputStream is =  url.openStream();
+        String val="";
+        try( BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                val+=line;
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new MalformedURLException("URL is malformed!!");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            throw new IOException();
+        }
+        return val;
+    }
+    public static String GetTemperature(String json){
+    String internalCode = json.substring(json.lastIndexOf("\"internal_id\":\"") + "\"internal_id\":\"".length(), json.length() - 1);
+    if (json.lastIndexOf("\"temp\":\"") == -1) return null;
+    return internalCode.substring(0, internalCode.indexOf("\""));
+}
+ 
+}
